370468ad10a2705857e953c7244272c26bf5a7d0
Kaustubh Joshi <poonamjoshi046@gmail.com>
2026-03-20 10:49:59 +0530

    Add getMinMax function to find min and max in array
    Implemented a function to find the minimum and maximum values in an array.

--- Experiment List - 19-01-2026 to 25-01-2026/2nd.py
+++ Experiment List - 19-01-2026 to 25-01-2026/2nd.py
@@ -1,1 +1,19 @@
+#Given an array arr[]. Your task is to find the minimum and maximum elements 
+#in the array.
+#Examples:
+#Input: arr[] = [1, 4, 3, 5, 8, 6]
+#Output: [1, 8]
+#Explanation: minimum and maximum elements of array are 1 and 8
 
+class Solution:
+    def getMinMax(self, arr):
+        min_val = arr[0]
+        max_val = arr[0]
+        
+        for num in arr:
+            if num < min_val:
+                min_val = num
+            if num > max_val:
+                max_val = num
+                
+        return [min_val, max_val]
