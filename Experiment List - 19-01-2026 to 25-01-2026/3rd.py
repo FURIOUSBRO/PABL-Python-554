828eb02a96dcdc0a88c401725ed1886b90d1087e
Kaustubh Joshi <poonamjoshi046@gmail.com>
2026-03-20 10:59:02 +0530

    Add kthSmallest function to find kth smallest element
    Implemented a function to find the kth smallest element in an array.

--- Experiment List - 19-01-2026 to 25-01-2026/3rd.py
+++ Experiment List - 19-01-2026 to 25-01-2026/3rd.py
@@ -1,1 +1,12 @@
 
+#Given an integer array arr[] and an integer k, your task is to find and return the kth smallest element in the given array.
+#Note: The kth smallest element is determined based on the sorted order of the array.
+#Examples :
+#Input: arr[] = [10, 5, 4, 3, 48, 6, 2, 33, 53, 10], k = 4
+#Output: 5
+#Explanation: 4th smallest element in the given array is 5.
+
+class Solution:
+    def kthSmallest(self, arr, k):
+        arr.sort()          # Sort the array in ascending order
+        return arr[k - 1]   # Return kth smallest element
